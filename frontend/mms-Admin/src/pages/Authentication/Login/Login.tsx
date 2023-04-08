a6234a99942e84b9b43a4dbf99225b372df2ae6a
tamsay <longetemitope@gmail.com>
2023-04-08 23:01:18 +0100

    style fix and adjustments

--- frontend/mms-Admin/src/pages/Authentication/Login/Login.tsx
+++ frontend/mms-Admin/src/pages/Authentication/Login/Login.tsx
@@ -3,7 +3,7 @@ import cx from "classnames";
 import styles from "./Login.module.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  return <div className={cx(styles.loginContainer)}>Login</div>;
 };
 
 export default Login;
