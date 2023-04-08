d2b3a56658ab45296e587f7d237409f6e265e0c2
tamsay <longetemitope@gmail.com>
2023-04-08 07:47:49 +0100

    initial project files update

--- frontend/mms-Admin/src/main.tsx
+++ frontend/mms-Admin/src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/fonts/Mukta-Regular.ttf";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
