3f277d31915cad5ea1965817cef47a47d2afc303
tamsay <longetemitope@gmail.com>
2023-04-08 22:38:09 +0100

    splash screen completed

--- frontend/mms-Admin/src/main.tsx
+++ frontend/mms-Admin/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/fonts/Mukta-Regular.ttf";
+
+import "./styles/global/flexAndGrid.scss";
+import "./styles/global/variables.scss";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
