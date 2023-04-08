616876e790ba7ce8e4c2413e0935575b62108152
tamsay <longetemitope@gmail.com>
2023-04-08 08:19:39 +0100

    login page created

---
+++ frontend/mms-Admin/src/pages/Authentication/Login/Login.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import cx from "classnames"
+import styles from "./Login.module.scss"
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
