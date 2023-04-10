55b5b17b00b09b4813a0b1619bfbbe99bf815b71
tamsay <longetemitope@gmail.com>
2023-04-10 19:47:25 +0100

    removal of the splash screen

--- frontend/mms-Admin/src/routes/Routes.tsx
+++ frontend/mms-Admin/src/routes/Routes.tsx
@@ -10,7 +10,7 @@ const RoutesComponent: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LandingPage />} />
+        <Route path='/' element={<Login />} />
         <Route path='/login' element={<Login />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
         <Route path='/reset-password' element={<ResetPassword />} />
