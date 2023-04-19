f0ba075391ca54655ed07a9cc345449b7a9fb9bf
John Kehinde <kehindejohn018@gmail.com>
2023-04-19 22:19:30 +0100

    added current user service

---
+++ backend/src/mms.Infrastructure/Implementation/CurrentUserService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using mms.Domain.Entities;
+using mms.Infrastructure.Context;
+using mms.Infrastructure.Interface;
+
+namespace mms.Infrastructure.Implementation
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SQLContext _context;
+
+        public CurrentUserService(
+            IHttpContextAccessor httpContextAccessor,
+            UserManager<AppUser> userManager,
+            ApplicationContext context)
+        {
+            AppUserId = httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            UserRole = httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Role);
+            UserEmail = httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Email);
+            FullName = httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.GivenName)
+                       + " " + httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Surname);
+            UserPhoneNumber = httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.MobilePhone);
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public Guid AppUserId { get; }
+        public string UserRole { get; }
+        public string UserEmail { get; }
+        public string FullName { get; }
+        public string UserPhoneNumber { get; set; }
+
+        public Task<AppUser> GetLoggedInUser()
+        {
+            return await _userManager.FindByIdAsync(userId: AppUserId);
+        }
+    }
+}
---
+++ backend/src/mms.Infrastructure/Interface/ICurrentUserService.cs
@@ -0,0 +1,15 @@
+﻿using mms.Domain.Entities;
+
+namespace mms.Infrastructure.Interface
+{
+    public interface ICurrentUserService
+    {
+        Guid AppUserId { get; }
+        string UserRole { get; }
+        string UserEmail { get; }
+        string UserPhoneNumber { get; set; }
+        string FullName { get; }
+
+        Task<AppUser> GetLoggedInUser();
+    }
+}
