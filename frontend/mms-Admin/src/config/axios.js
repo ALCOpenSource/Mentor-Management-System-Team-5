551bbbb2256cca6f0405677421bb8723adfba6c9
tamsay <longetemitope@gmail.com>
2023-04-19 17:40:35 +0100

    style fix

--- frontend/mms-Admin/src/config/axios.js
+++ frontend/mms-Admin/src/config/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { getToken } from "@/utils/auth";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL
 });
 
 instance.interceptors.request.use(
