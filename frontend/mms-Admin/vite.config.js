72ef87df9a895ab33cec9f86b2468d85ae24ff74
Calistus Igwilo <ebunilo@yahoo.com>
2023-04-21 12:45:40 +0100

    improve code style

--- frontend/mms-Admin/vite.config.js
+++ frontend/mms-Admin/vite.config.js
@@ -7,8 +7,8 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-        host: '0.0.0.0',
-        port: 3000, // The host port the app will be accessed from
+        host: "0.0.0.0",
+        port: 3000 // The host port the app will be accessed from
     },
     plugins: [react(), svgr()],
     resolve: {
