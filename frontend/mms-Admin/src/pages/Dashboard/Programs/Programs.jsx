d8728d640bb2f834c21488ef4eb2027f960a591b
tamsay <longetemitope@gmail.com>
2023-04-22 18:44:16 +0100

    commencement of the program feature

--- frontend/mms-Admin/src/pages/Dashboard/Programs/Programs.jsx
+++ frontend/mms-Admin/src/pages/Dashboard/Programs/Programs.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import cx from "classnames";
+import styles from "./Programs.module.scss";
 
 function Programs() {
-  return <div>Programs</div>;
+  return <div className={cx(styles.programsContainer, "flexCol")}>Programs</div>;
 }
 
 export default Programs;
