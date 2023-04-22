2183c4a10179d84f0651129cc054abd48a4c0c05
Calistus Igwilo <ebunilo@yahoo.com>
2023-04-22 18:23:01 +0100

    lint code

--- frontend/mms-Admin/vite.config.js
+++ frontend/mms-Admin/vite.config.js
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import svgr from 'vite-plugin-svgr';
-
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        host: "0.0.0.0",
-        port: 3000 // The host port the app will be accessed from
-    },
-    plugins: [react(), svgr()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src")
-        }
+  server: {
+    host: "0.0.0.0",
+    port: 3000 // The host port the app will be accessed from
+  },
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
     }
+  }
 });
