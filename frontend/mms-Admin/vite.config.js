ea552edec4d6e29242b8555a243227a1a1faa07c
Calistus Igwilo <ebunilo@yahoo.com>
2023-04-23 08:00:45 +0100

    implement ENV variables

--- frontend/mms-Admin/vite.config.js
+++ frontend/mms-Admin/vite.config.js
@@ -5,14 +5,14 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 3000, // The host port the app will be accessed from
-    },
-    plugins: [react(), svgr()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src")
-        }
+  server: {
+    host: import.meta.env.VITE_HOST,
+    port: import.meta.env.VITE_PORT // The host port the app will be accessed from
+  },
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
     }
+  }
 });
