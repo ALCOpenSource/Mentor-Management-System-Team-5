b35182b6a2542f07eee9950663c1779bacd65487
tamsay <longetemitope@gmail.com>
2023-04-25 21:44:23 +0100

    pulling latest files and linting update

--- frontend/mms-Admin/src/components/ToggleSwitch/ToggleSwitch.jsx
+++ frontend/mms-Admin/src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -22,30 +22,26 @@ function ToggleSwitch({ onChange, initialState }) {
       offColor='#b3b3b3'
       checkedIcon={false}
       uncheckedIcon={false}
-      checkedHandleIcon={
-        <div
+      checkedHandleIcon={<div
           style={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
             height: "100%"
           }}
-        >
+                         >
           <ToggleOnIcon />
-        </div>
-      }
-      uncheckedHandleIcon={
-        <div
+        </div>}
+      uncheckedHandleIcon={<div
           style={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
             height: "100%"
           }}
-        >
+                           >
           <ToggleOffIcon />
-        </div>
-      }
+        </div>}
       className='react-switch'
     />
   );
