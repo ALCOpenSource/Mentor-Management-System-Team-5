b7b6e136b2918e66d3862f11eaa02990715b0d96
John Kehinde <kehindejohn018@gmail.com>
2023-04-30 10:29:41 +0100

    Added comment to notification

---
+++ backend/src/mms.Infrastructure/Migrations/20230430092648_AddedComments.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mms.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedComments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CommentsEmail",
+                table: "UserNotifications",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CommentsInApp",
+                table: "UserNotifications",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CommentsEmail",
+                table: "UserNotifications");
+
+            migrationBuilder.DropColumn(
+                name: "CommentsInApp",
+                table: "UserNotifications");
+        }
+    }
+}
