64f699eacb26c23436ac2ad65869d5b1d04caf32
ammwangi <elijah.mwangi@kamsware.com>
2023-05-08 21:48:28 +0300

    Create programme

--- backend/src/mms.api/Controllers/ProgrammeController.cs
+++ backend/src/mms.api/Controllers/ProgrammeController.cs
@@ -1,14 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using mms.Application.Programme.Command;
 using mms.Application.Programme.Query;
+using mms.Application.Report.Command;
 
 namespace mms.api.Controllers
 {
     public class ProgrammeController:BaseController
     {
-        [HttpGet("programmes")]
+        [HttpGet("programme")]
         public async Task<IActionResult> GetProgrammes()
         {
             var result = await Mediator.Send(new GetProgrammesCommand());
+            if (result.Succeeded == false)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost("programme")]
+        public async Task<IActionResult> CreateTask(CreateProgrammeCommand command)
+        {
+            var result = await Mediator.Send(command);
             if (result.Succeeded == false)
             {
                 return BadRequest(result);
