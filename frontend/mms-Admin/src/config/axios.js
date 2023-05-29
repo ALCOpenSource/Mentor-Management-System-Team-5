cd8eb6c17f662caa8b0500d878b14373d8b93d57
tamsay <longetemitope@gmail.com>
2023-05-29 21:50:53 +0100

    updated the axios instance to include content type

--- frontend/mms-Admin/src/config/axios.js
+++ frontend/mms-Admin/src/config/axios.js
@@ -4,7 +4,10 @@ import { getToken } from "@/utils/auth";
 import { refreshAccessToken } from "@/redux/Auth/AuthSlice";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 instance.interceptors.request.use(
