62e46fe98208c97cf49cb1fd5401d9dbff479b97
ammwangi <elijah.mwangi@kamsware.com>
2023-06-09 09:52:56 +0300

    pre virtual test

--- backend/src/mms.Domain/Common/BaseEntity.cs
+++ backend/src/mms.Domain/Common/BaseEntity.cs
@@ -5,7 +5,8 @@ namespace mms.Domain.Common
 {
     public class BaseEntity
     {
-        [Key] public string Id { get; set; } = Guid.NewGuid().ToString();
+        [Key] 
+        public string Id { get; set; } = Guid.NewGuid().ToString();
 
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
