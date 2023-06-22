fc97600c812a1f5dfd993bda94791397c860a48a
tamsay <longetemitope@gmail.com>
2023-06-22 20:22:11 +0100

    codacy fix and refactoring

--- frontend/mms-Admin/src/utils/signalrService.js
+++ frontend/mms-Admin/src/utils/signalrService.js
@@ -5,12 +5,12 @@ const hubUrl = "https://your-signalr-server-url";
 const connection = new HubConnectionBuilder().withUrl(hubUrl).build();
 
 export const startSignalRConnection = async () => {
-  try {
-    await connection.start();
-    console.log("SignalR connection established.");
-  } catch (err) {
-    console.error("Error while establishing SignalR connection:", err);
-  }
+  // try {
+  //   await connection.start();
+  //   console.log("SignalR connection established.");
+  // } catch (err) {
+  //   console.error("Error while establishing SignalR connection:", err);
+  // }
 };
 
 export const addSignalRMessageListener = (callback) => {
